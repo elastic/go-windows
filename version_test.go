eca15843cf7ee7bbffa6761b83a630cf96ed16ab
Andrew Kroh <andrew.kroh@elastic.co>
2018-02-07 12:23:58 -0500

    Add GetFileVersionInfo and related function
    This adds GetFileVersionInfo, GetFileVersionInfoSize, and VerQueryValue.

---
+++ version_test.go
@@ -0,0 +1,42 @@
+// Copyright 2018 Elasticsearch Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build windows
+
+package windows
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetFileVersion(t *testing.T) {
+	versionData, err := GetFileVersionInfo(`C:\Windows\System32\ntoskrnl.exe`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	info, err := versionData.FixedFileInfo()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.NotZero(t, info)
+
+	productVersion, err := versionData.QueryValue("ProductVersion")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Contains(t, productVersion, info.ProductVersion())
+}
