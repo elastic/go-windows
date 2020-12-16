2fcbfd2d6cc477cf6bd8d0de1341ea4ef976cbdb
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-12-16 11:01:00 +0000

    Add more debugging details

--- .ci/scripts/test.bat
+++ .ci/scripts/test.bat
@@ -1,4 +1,7 @@
-GO111MODULE=off go get -u github.com/elastic/go-licenser
+set
+set GO111MODULE=off
+go get -u github.com/elastic/go-licenser
+set GO111MODULE=on
 go mod verify
 go-licenser -d
 go run .ci/scripts/check_format.go
